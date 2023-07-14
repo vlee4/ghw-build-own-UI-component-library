c322ecda250d873b7ea8a960243d19cc9eb3edb7
vlee4 <vicglee1234007@gmail.com>
2023-07-14 02:09:25 -0500

    Configure project as a library for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, '/src/Components/index.ts'),
+      name: 'ghw-components-library-vl-test',
+      // the proper extensions will be added
+      fileName: 'ghw-components-library-vl-test',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+  }
 })
